17ee3e18939aeb5d0559bf77f08fbccb064a839f
Nikita M <nm.mikhaylov@gmail.com>
2018-02-09 14:20:14 +0300

    added sidebar

--- ui.R
+++ ui.R
@@ -1,9 +1,22 @@
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+library(dygraphs)
 
-header <-  dashboardHeader()
-bar <- dashboardSidebar()
-body <- dashboardBody()
+header <-  dashboardHeader(titleWidth = 180, title = "")
+bar <- dashboardSidebar(width = 180,
+          sidebarMenu( br(),
+              menuItem("Временной ряд", tabName = "timeSerie", icon = icon("line-chart")),
+              #menuItem("Анализ текста", tabName = "textMining", icon = icon("list")),
+              menuItem("Набор данных", tabName = "textData", icon = icon("table"))))
+
+body <- dashboardBody(
+        tags$head(
+          tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+          )
+  
+  
+)
 
 dashboardPage(header, bar, body)
 
